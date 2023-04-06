f3cf32880ca50e6d5513809e43e6fbbf61339ad2
a1e0heewon <a1e0heewon@a1e0heewonui-MacBookPro.local>
2023-04-06 23:00:44 +0900

    feat(route): 페이지 라우팅

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import PrivateRoute from 'utils/PrivateRoute';
+import Main from 'pages/Main';
+import SignIn from 'pages/SignIn';
+import SignUp from 'pages/SignUp';
+import Todo from 'pages/Todo';
 
 function App() {
-  return <div />;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Main />} />
+                <Route element={<PrivateRoute authentication={false} />}>
+                    <Route path="/signin" element={<SignIn />} />
+                    <Route path="/signup" element={<SignUp />} />
+                </Route>
+                <Route element={<PrivateRoute authentication />}>
+                    <Route path="/todo" element={<Todo />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
