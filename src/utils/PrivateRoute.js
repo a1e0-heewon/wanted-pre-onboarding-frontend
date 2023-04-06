04c2aecc6701bbd3ccfb4bbd0ead5573da720ade
a1e0heewon <a1e0heewon@a1e0heewonui-MacBookPro.local>
2023-04-06 22:49:25 +0900

    fix(auth): PrivateRoute 수정

--- src/utils/PrivateRoute.js
+++ src/utils/PrivateRoute.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
-export function PrivateRoute() {
+export function PrivateRoute(authentication) {
     const isAuthenticated = localStorage.getItem('accesstoken');
 
+    if (authentication) {
+        return isAuthenticated === null || isAuthenticated === 'false' ? (
+            <Navigate to="/login" />
+        ) : (
+            <Outlet />
+        );
+    }
     return isAuthenticated === null || isAuthenticated === 'false' ? (
-        <Navigate to="/login" />
-    ) : (
         <Outlet />
+    ) : (
+        <Navigate to="/" />
     );
 }
