23a8c564e8eed3e5542a4cffce33517eef4853b9
a1e0heewon <a1e0heewon@a1e0heewonui-MacBookPro.local>
2023-04-06 23:29:45 +0900

    fix(route): 리다이렉션 페이지 수정

--- src/utils/PrivateRoute.js
+++ src/utils/PrivateRoute.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
-export function PrivateRoute(authentication) {
+export default function PrivateRoute(authentication) {
     const isAuthenticated = localStorage.getItem('accesstoken');
 
     if (authentication) {
         return isAuthenticated === null || isAuthenticated === 'false' ? (
-            <Navigate to="/login" />
+            <Navigate to="/signin" />
         ) : (
             <Outlet />
         );
